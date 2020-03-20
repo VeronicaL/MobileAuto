5fc9d0e0c5f006898e6d068a75116607971702ba
Veronica148 <Veronica_Lapunka@epam.com>
2020-03-20 15:24:13 +0300

    Task3

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -3,9 +3,16 @@ import org.junit.Test;
 
 public class MainClassTest {
 
+
     @Test
-    public void testGetClassNumber(){
-        MainClass number = new MainClass();
-        Assert.assertTrue("Returned value is less or equal 45", number.getClassNumber() > 45);
+    public void testGetClassString(){
+        MainClass stringFromMain = new MainClass();
+
+        String str = stringFromMain.getClassString();
+        String sub1 = "hello";
+        String sub2 = "Hello";
+        Assert.assertTrue(String.format("String '%s' doesn't contain '%s' or '%s'", str, sub1, sub2),
+                str.contains(sub1) || str.contains(sub2));
     }
 }
+
