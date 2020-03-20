1f3f6a35a38ffcdeac4a2ddf236411bb8180c98c
Veronica148 <Veronica_Lapunka@epam.com>
2020-03-20 15:22:42 +0300

    Task2

--- JavaAppiumAutomation/src/MainClassTest.java
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 public class MainClassTest {
 
     @Test
-    public void testGetLocalNumber(){
-        MainClass func = new MainClass();
-        Assert.assertTrue("Result of getLocalNumber() function is not 14!", func.getLocalNumber() == 14);
+    public void testGetClassNumber(){
+        MainClass number = new MainClass();
+        Assert.assertTrue("Returned value is less or equal 45", number.getClassNumber() > 45);
     }
 }
