0f955cbab050d0c691a64a1d90e16825d7448447
VeronicaL <vika_148@mail.ru>
2020-04-23 10:09:46 +0300

    Added test for iOS.

---
+++ JavaAppiumAutomation/src/lib/iOSTestCase.java
@@ -0,0 +1,47 @@
+package lib;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import junit.framework.TestCase;
+import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class iOSTestCase extends TestCase {
+
+    protected AppiumDriver driver;
+    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","iOS");
+        capabilities.setCapability("deviceName","iPhone SE");
+        capabilities.setCapability("platformVersion","11.3");
+        capabilities.setCapability("app","/Users/veronica/!Тренинги/mobileAuto/git/JavaAppiumAutomation/apks/Wikipedia.app");
+
+        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        this.rotateScreenPortrait();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        driver.quit();
+        super.tearDown();
+    }
+
+    protected void rotateScreenPortrait(){
+        driver.rotate(ScreenOrientation.PORTRAIT);
+    }
+
+    protected void rotateScreenLandscape(){
+        driver.rotate(ScreenOrientation.LANDSCAPE);
+    }
+
+    protected void backgroundApp(int seconds){
+        driver.runAppInBackground(seconds);
+    }
+
+}
---
+++ JavaAppiumAutomation/src/tests/iOS/GetStartedTest.java
@@ -0,0 +1,24 @@
+package tests.iOS;
+
+import lib.iOSTestCase;
+import lib.ui.WelcomePageObject;
+import org.junit.Test;
+
+public class GetStartedTest extends iOSTestCase {
+    @Test
+    public void testPassThroughWelcome(){
+        WelcomePageObject welcomePageObject = new WelcomePageObject(driver);
+        welcomePageObject.waitForLearnMoreLink();
+        welcomePageObject.clickNextButton();
+
+        welcomePageObject.waitForNewWayToExploreText();
+        welcomePageObject.clickNextButton();
+
+        welcomePageObject.waitForAddOrEditPreferredLangText();
+        welcomePageObject.clickNextButton();
+
+        welcomePageObject.waitForlearnMoreAboutDataCollectedText();
+        welcomePageObject.clickGetStartedButton();
+    }
+
+}
